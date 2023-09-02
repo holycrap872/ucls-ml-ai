2289531b3a5795bfcc6a59da6719a895359b49ec
Eric Rizzi <eric.rizzi@protonmail.com>
2023-09-02 10:22:11 -0500

    Lesson for introducing graphs

---
+++ CourseMaterial/02_intro_to_data_sci/00_funny_graphs/funny_graphs.py
@@ -0,0 +1,76 @@
+#!/usr/bin/python3
+import typing
+
+import matplotlib
+import matplotlib.pyplot as plots
+
+plots.style.use("fivethirtyeight")
+matplotlib.use("MacOSX")
+
+
+def funny_bar_chart() -> None:
+    bar_names = ["Confirmed", "Refuted"]
+    bar_values = [0, 100]
+
+    plots.figure(figsize=(6, 6))
+
+    plots.bar(x=bar_names, height=bar_values)
+
+    plots.title("Claims of Super Natural Powers (xkcd)")
+    plots.grid(False)
+    plots.yticks([1])
+
+    plots.show()
+
+
+def funny_scatter_plot() -> None:
+    plots.figure(figsize=(6, 6))
+
+    plots.scatter([2.5, 3, 2, 1], [10, 9.5, 9, 10], color="red", label="Infant")
+    plots.scatter([14, 12.5, 14.5, 13.5, 14], [1, 0, 0, 0.5, 0.5], color="orange", label="Teen")
+    plots.scatter([25, 24, 23, 22], [0, 0.5, 0, 1], color="yellow", label="Hipster")
+    plots.scatter([40, 37, 36, 39], [7, 2, 9, 2.5], color="green", label="Parent")
+    plots.scatter([60, 62, 65, 66], [3, 2.5, 8.1, 8.7], color="blue", label="Retirement")
+    plots.scatter([80, 81, 82, 83], [10, 9.5, 10, 9], color="purple", label="Elderly")
+
+    plots.ylabel("Ability to Sit and Smile")
+    plots.xlabel("Life Stage")
+    plots.legend()
+    plots.grid(True)
+
+    plots.show()
+
+
+def is_day(hours_since_start: int) -> bool:
+    hour = hours_since_start % 24
+    if 6 < hour < 22:
+        return True
+    return False
+
+
+def funny_line_graph() -> None:
+    plots.figure(figsize=(15, 8))
+
+    hours = [x for x in range(0, 100)]
+    fondness = []
+    for hour in hours:
+        if is_day(hour):
+            fondness.append(hour)
+        else:
+            fondness.append(0)
+
+    plots.plot(hours, fondness)
+
+    plots.title("Absense Makes the Heart Grow Fond (except when I'm sleeping)")
+    plots.ylabel("Amount of Fondness")
+    plots.xlabel("Amount of Absence")
+    plots.legend()
+    plots.grid(True)
+
+    plots.show()
+
+
+if __name__ == "__main__":
+    funny_bar_chart()
+    funny_scatter_plot()
+    funny_line_graph()
---
+++ CourseMaterial/02_intro_to_data_sci/00_funny_graphs/lesson_plan.md
@@ -0,0 +1,48 @@
+## Lesson Plan
+
+## Setup
+
+Distribute base python file for every one to experiment with / modify
+
+## Actual Lesson
+
+- Reflection
+     - What did we do last time?
+- Today, going to work on graphing
+    - Funny graphs to show
+        - Bar Charts:
+            - Dangers
+                - "Died in a XXX accidents"
+                - https://xkcd.com/369/
+            - First Post
+                - Joke about danger of comment boards
+                - https://xkcd.com/1019/
+            - 11th Grade
+                - Perl joke
+                - https://xkcd.com/519/
+        - Scatter Plot
+            - Things you learn
+                - Growing up joke
+                - "https://xkcd.com/1775/"
+            - Real Estate Analysis
+                - Joke about walkability of planets
+                - https://xkcd.com/2796/
+        - Line Graph
+            - Conversations
+                - Joke about time spent on toilet over time
+                - https://xkcd.com/646/
+            - Escalators
+                - Joke about how much want to go up escalator
+                - https://xkcd.com/252/
+        - Overlapping Circles
+            - Set theory joke
+            - https://xkcd.com/2769/
+- Show my code and discuss a bit
+    - see `funny_graphs.py`
+        - Amount of "funniness" may vary
+    - Show common mistakes
+        - x/y arrays of different size
+        - misspelled colors
+        - Bad labels...
+- Create three joke graphs, one of each type
+- Will show them at the end of class (maybe next week)?
