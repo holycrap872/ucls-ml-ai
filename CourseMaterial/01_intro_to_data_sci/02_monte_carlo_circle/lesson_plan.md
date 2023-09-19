8aa65b7db0080661c1d4e323d896bebf3811d782
Eric Rizzi <eric.rizzi@gmail.com>
2023-09-19 07:55:19 -0500

    Create plan (and reorder) for monte carlo lesson

---
+++ CourseMaterial/01_intro_to_data_sci/02_monte_carlo_circle/lesson_plan.md
@@ -0,0 +1,32 @@
+## Essential Questions
+
+## Lesson Plan
+
+### Setup
+
+None
+
+### Actual Lesson
+
+- Review
+    - Libraries
+    - Installing libraries
+    - Show documentation for libraries
+- Today going to graph the monte carlo method
+- Anyone heard of monte carlo analysis?
+    - Bunch of random points
+    - Demonstrate by drawing a circle on a piece of paper and dropping pencil
+    - Can be used to find very good approximation of very difficult shapes
+- For example
+    - Find area under x^2 curve
+    - How would we do this?
+        - Plan out/discuss together
+    - Show code and run
+    - That's calculus!
+- Today going to do Monte Carlo area of a circle
+    - How would we do this?
+        - Plan out/discuss together
+
+### Homework
+
+None
---
+++ CourseMaterial/01_intro_to_data_sci/02_monte_carlo_circle/monte_curve.py
@@ -0,0 +1,67 @@
+import random
+import typing
+
+import matplotlib
+import matplotlib.pyplot as plots
+
+plots.style.use("fivethirtyeight")
+matplotlib.use("MacOSX")
+
+"""
+Find the area under x^2 curve from -1 to 1
+"""
+
+
+def get_random_point() -> typing.Tuple[float, float]:
+    rand_x = (random.random() * 2.0) - 1.0  # random number between -1 and 1
+    rand_y = random.random()  # random number between 0 and 1
+    return (rand_x, rand_y)
+
+
+def is_under_curve(point: typing.Tuple[float, float]) -> bool:
+    assert -1.0 <= point[0] <= 1.0
+    assert -1.0 <= point[1] <= 1.0
+
+    calc_y = point[0] ** 2
+    return point[1] < calc_y
+
+
+def run_experiment(trials: int, graph_ites: int) -> None:
+    in_x_points: typing.List[float] = []
+    in_y_points: typing.List[float] = []
+    out_x_points: typing.List[float] = []
+    out_y_points: typing.List[float] = []
+
+    running_best_guess = []
+    total_in_circle = 0
+
+    for trial in range(1, trials):
+        random_point = get_random_point()
+        if is_under_curve(random_point):
+            total_in_circle += 1
+
+            in_x_points += [random_point[0]]
+            in_y_points += [random_point[1]]
+        else:
+            out_x_points += [random_point[0]]
+            out_y_points += [random_point[1]]
+
+        if trial % graph_ites == 0:
+            running_best_guess += [(total_in_circle / trial) * 2]
+            print(running_best_guess[-1])
+
+    plots.figure(figsize=(6, 6))
+    plots.scatter(in_x_points, in_y_points, color="darkblue", label="Inside circle")
+    plots.scatter(out_x_points, out_y_points, color="gold", label="Outside circle")
+    plots.xlabel("X")
+    plots.xlabel("Y")
+    plots.show()
+
+    plots.figure(figsize=(12, 6))
+    plots.plot(running_best_guess)
+    plots.xlabel("Iteration Count")
+    plots.xlabel("Estimated Value Under Curve")
+    plots.show()
+
+
+run_experiment(5000, 10)
