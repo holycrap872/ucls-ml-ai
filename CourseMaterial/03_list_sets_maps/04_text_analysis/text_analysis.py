78c44f9513b8644a7d6d000bf16479ee6d1c46be
Eric Rizzi <eric.rizzi@gmail.com>
2024-12-01 19:02:38 -0600

    Improve "solution" `text_analysis.py` to match worksheet

--- CourseMaterial/03_list_sets_maps/04_text_analysis/text_analysis.py
+++ CourseMaterial/03_list_sets_maps/04_text_analysis/text_analysis.py
@@ -1,28 +1,27 @@
 #!/usr/bin/env python3
-import typing
 
 
-def load_book(path: str) -> typing.List[str]:
+def open_file(path: str) -> str:
     with open(path, "r") as fp:
         text = fp.read()
-        text = text.lower()
-        text = text.replace(".", "")
-        text = text.replace(",", "")
-        text = text.replace("_", "")
-        text = text.replace("'", "")
-        text = text.replace('"', "")
-        text = text.replace("?", "")
-        text = text.replace("!", "")
-        text = text.replace("-", " ")
-        return text.split()
+        return text
 
 
-def get_num_words(text: typing.List[str]) -> int:
-    return len(text)
+def normalize(text: str) -> list[str]:
+    text = text.lower()
+    text = text.replace(".", "")
+    text = text.replace(",", "")
+    text = text.replace("_", "")
+    text = text.replace("'", "")
+    text = text.replace('"', "")
+    text = text.replace("?", "")
+    text = text.replace("!", "")
+    text = text.replace("-", " ")
+    return text.split()
 
 
-def create_count_dict(text: typing.List[str]) -> typing.Dict[str, int]:
-    count_dict = {}
+def create_count_dict(text: list[str]) -> dict[str, int]:
+    count_dict: dict[str, int] = {}
     for word in text:
         if word not in count_dict:
             count_dict[word] = 0
@@ -31,53 +30,46 @@ def create_count_dict(text: typing.List[str]) -> typing.Dict[str, int]:
     return count_dict
 
 
-def max_find(count_dict: typing.Dict[str, int]) -> str:
-    max_count = 0
-    word = ""
+def max_find(count_dict: dict[str, int]) -> str | None:
+    max_count = None
+    word = None
 
     for k in count_dict:
-        if count_dict[k] > max_count:
+        if max_count is None or count_dict[k] > max_count:
             max_count = count_dict[k]
             word = k
-
-        if count_dict[k] == 1:
-            print(k)
 
     return word
 
 
-def jaccard_similarity(base_book_paths: typing.List[str], compare_book_path: str) -> float:
+def jaccard_similarity(base_book_paths: list[str], compare_book_path: str) -> float:
     common_words = None
     for base_book_path in base_book_paths:
-        words_in_book = set(load_book(base_book_path))
+        words_in_book = set(open_file(base_book_path))
         if common_words is None:
             common_words = words_in_book
         else:
             common_words = common_words.intersection(words_in_book)
 
-    compare_book_words = set(load_book(compare_book_path))
+    compare_book_words = set(normalize(open_file(compare_book_path)))
     assert common_words is not None
 
     return len(common_words.intersection(compare_book_words)) / len(common_words.union(compare_book_words))
 
 
 if __name__ == "__main__":
-    rb_word_list = load_book("CourseMaterial/data/robert_frost.txt")
-    fr_word_list = load_book("CourseMaterial/data/frankenstein.txt")
-
-    print("Words in Robert Frost:", get_num_words(rb_word_list))
-    print("Words in Frankenstein:", get_num_words(rb_word_list))
-
-    rb_count_dict = create_count_dict(rb_word_list)
-    fr_count_dict = create_count_dict(fr_word_list)
+    rb_word_list = normalize(open_file("CourseMaterial/data/robert_frost.txt"))
+    print(f"Words in Robert Frost: {len(rb_word_list)}")
 
     rb_word_set = set(rb_word_list)
-    fr_word_set = set(fr_word_list)
+    print(f"Unique words in Robert Frost: {len(rb_word_set)}")
 
-    print("Unique words in Robert Frost:", len(rb_word_set))
-    print("Unique words in Frankenstein:", len(fr_word_set))
+    rb_count_dict = create_count_dict(rb_word_list)
+    most_frequent_rb_word = max_find(rb_count_dict)
+    print(f"Most frequent word in Robert Frost: {most_frequent_rb_word}")
 
-    print("Words in either RF and Frank:", len(rb_word_set.union(fr_word_list)))
+    fr_word_list = set(normalize(open_file("CourseMaterial/data/frankenstein.txt")))
+    print(f"Words in either RF and Frank: {len(rb_word_set.union(fr_word_list))}")
     print("Words in both RF and Frank:", len(rb_word_set.intersection(fr_word_list)))
 
     similarity_thriller = jaccard_similarity(
